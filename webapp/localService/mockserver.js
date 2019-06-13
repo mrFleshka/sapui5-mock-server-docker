7ae239812c032327666a700dbbd20928e42deb91
mrFleshka <mrFleshka@gmail.com>
2019-06-13 16:26:25 +0300

    Step 4: Calling a Function Import

--- webapp/localService/mockserver.js
+++ webapp/localService/mockserver.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/util/MockServer"
-], function(MockServer) {
+    "sap/ui/thirdparty/jquery",
+    "sap/ui/core/util/MockServer",
+    "sap/base/Log"
+], function(jQuery, MockServer, Log) {
     "use strict";
 
     return {
@@ -22,6 +24,30 @@ sap.ui.define([
                 bGenerateMissingMockData: true
             });
 
+            // handling mocking a function import call step
+            var aRequests = oMockServer.getRequests();
+            aRequests.push({
+                method: "GET",
+                path: new RegExp("FindUpcomingMeetups(.*)"),
+                response: function(oXhr) {
+                    Log.debug("Incoming request for FindUpcomingMeetups");
+                    var today = new Date();
+                    today.setHours(0); // or today.toUTCString(0) due to timezone differences
+                    today.setMinutes(0);
+                    today.setSeconds(0);
+                    jQuery.ajax({
+                        url: "/Meetups?$filter=EventDate ge " + "/Date(" + today.getTime() + ")/",
+                        dataType : 'json',
+                        async: false,
+                        success : function(oData) {
+                            oXhr.respondJSON(200, {}, JSON.stringify(oData));
+                        }
+                    });
+                    return true;
+                }
+            });
+            oMockServer.setRequests(aRequests);
+
             // handling custom URL parameter step
             var fnCustom = function(oEvent) {
                 var oXhr = oEvent.getParameter("oXhr");
@@ -34,7 +60,7 @@ sap.ui.define([
             // start
             oMockServer.start();
 
-            jQuery.sap.log.info("Running the app with mock data");
+            Log.info("Running the app with mock data");
         }
 
     };
